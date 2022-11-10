ded77c0686317f73c7c7fba7fc23dd673d6e2589
Dishant Dilip Yashwante <113447476+dishant1996@users.noreply.github.com>
2022-11-10 21:44:56 +0530

    GTP Assignment-13

---
+++ UC-3-R2/UC3-Refactor2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Generic
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int output = MaximumNumber<int>.GetMaximum(11, 22, 33);
+            Console.WriteLine($"max={output}");
+            float output1 = MaximumNumber<float>.GetMaximum(11.5f, 22.5f, 33.5f);
+            Console.WriteLine($"max={output1}");
+            string output2 = MaximumNumber<string>.GetMaximum("abc", "xyz", "pqr");
+            Console.WriteLine($"max={output2}");
+        }
+
+    }
+}
